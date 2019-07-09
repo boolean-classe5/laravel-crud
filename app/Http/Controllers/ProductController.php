218ba7e748b15a32a2fab8d84e1cc887af97e1a4
sofia <sofia.perlari@gmail.com>
2019-07-09 12:56:42 +0200

    test commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,6 +11,7 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::orderBy('id', 'desc')->get();
+        echo 'test';
 
         return view('products.index', compact('products'));
     }
